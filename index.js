64aa08c7f7eb36e0bcb746f05e1b6fc7f4f99e09
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-04 00:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ let dodger = document.getElementById("dodger");
 
 function moveDodgerLeft()
 {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
   
+  if (left > 0)
+  {
+    dodge.style.left = `${left - 1}px`;
+  }
 }
